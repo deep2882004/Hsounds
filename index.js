0fef485a455baf132d46c9230aa956d249979f48
deep2882004 <67994719+deep2882004@users.noreply.github.com>
2024-08-25 21:48:32 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,115 @@
+
+
+ScrollReveal({ 
+  reset: true,
+  distance:'60px',
+  duration:2500,
+  delay:400 
+ });
+ 
+ ScrollReveal().reveal('.top-banner', { delay: 500,origin:'top' });
+ ScrollReveal().reveal('.left-text', { delay: 500,origin:'right' });
+ ScrollReveal().reveal('.right-img', { delay: 500,origin:'top' });
+ ScrollReveal().reveal('#shop', { delay: 500,origin:'left', interval: 200 });
+ ScrollReveal().reveal('.box', { delay: 500,origin:'top' ,interval:200 });
+ ScrollReveal().reveal('#Product', { delay: 500,origin:'top' });
+ ScrollReveal().reveal('.content3', { delay: 500,origin:'bottom' });
+ ScrollReveal().reveal('.content-3-text', { delay: 500,origin:'left' });
+ ScrollReveal().reveal('.content-3-right-img', { delay: 500,origin:'right' });
+ 
+
+
+ ScrollReveal().reveal('.content-img-first-line2', { delay: 500,origin:'right' });
+ ScrollReveal().reveal('.nav-link', { delay: 400,origin:'top' });
+ ScrollReveal().reveal('#handi', { delay: 500,origin:'top' });
+ ScrollReveal().reveal('.content-img-first-line', { delay: 500,origin:'left' });
+ ScrollReveal().reveal('.content-img-first-line1', { delay: 500,origin:'left' });
+ ScrollReveal().reveal('.content-img-first-line2', { delay: 500,origin:'right' });
+ ScrollReveal().reveal('.content5-1', { delay: 500,origin:'left' });
+ ScrollReveal().reveal('.content5-2', { delay: 500,origin:'right' });
+ ScrollReveal().reveal('.content5-3', { delay: 500,origin:'left' });
+ ScrollReveal().reveal('.content5-4', { delay: 500,origin:'right' });
+ ScrollReveal().reveal('.company-text', { delay: 500,origin:'left' });
+ ScrollReveal().reveal('.line', { delay: 500,origin:'left' });
+
+
+window.onload = function(){
+
+
+
+var text = document.querySelector(".text-animation").children;
+textL = text.length;
+var value = 0;
+
+function animationText(){
+for(var i=0;i<textL;i++){
+   text[i].classList.remove("text-in");
+
+}
+ text[value].classList.add("text-in");
+if(value == textL-1){
+    value = 0;
+}
+
+else {
+    value++;
+}
+ 
+
+  setTimeout(animationText,3000);
+}
+
+
+animationText();
+
+
+var animation1 = document.querySelector(".content-3-animation").children;
+
+animation12 = animation1.length;
+
+animationValue = 0;
+
+function content3animation(){
+  for( var i=0;i<animation12;i++){
+    animation1[i].classList.remove("content-3-text-animation");
+    
+  }
+
+  animation1[animationValue].classList.add("content-3-text-animation");
+
+  if(animationValue == animation12-1){
+     animationValue = 0;
+
+  }
+
+  else{
+    animationValue++
+  }
+
+  
+
+  setTimeout(content3animation,2500)
+
+}
+content3animation();
+}
+
+
+var myhtml = document.querySelector("button");
+var myhtml1 = document.querySelector("#changeletter");
+
+ myhtml.addEventListener("click",function(){
+    myhtml.innerHTML = "subscribed";
+    myhtml.style.color="red";
+    myhtml.style.backgroundColor="white";
+    myhtml1.innerHTML = "Thank You For Subscribtion You Will Recive the Newsletter";
+
+ })
+
+ var hamberger = document.querySelector(".touglebtn");
+ var navlinks = document.querySelector(".nav-links");
+
+ hamberger.addEventListener("click",function(){
+  navlinks.classList.toggle("active");
+
+ })
